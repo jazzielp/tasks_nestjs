7d767e3efd44313118261e3e63f939c5c9fb4abc
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-05-26 10:16:23 -0700

    Creating the user, task, and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 import * as Joi from 'Joi';
 
 @Module({
@@ -11,6 +14,9 @@ import * as Joi from 'Joi';
         PORT: Joi.number().required(),
       }),
     }),
+    UserModule,
+    TaskModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
