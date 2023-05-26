e987ddd8f726fe0a0b653766722fb0bbf7f05943
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-05-26 16:34:51 -0700

    Configuring a log for events in the terminal and versioning our project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config/dist';
+import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use(morgan('dev'));
   const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api/v1');
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
